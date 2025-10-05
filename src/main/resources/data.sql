d929f673b9d61e84a2ce96fa6aa3d5a1c4739dae
jsalvaguitian <jsalvaguitian@frba.utn.edu.ar>
2025-10-05 16:33:37 -0300

    ya ni me acuerdo q mejore. agregue 3 datos en data.sql

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-/*INSERT INTO
-    Usuario(id, email, password, rol, activo)
-VALUES
-    (1, 'admin@unlam.edu.ar', 'admin', 'ADMINISTRADOR', true),
-    (2, 'cliente@unlam.edu.ar', 'cliente', 'CLIENTE', true),
-    (3, 'proveedor@unlam.edu.ar', 'proveedor', 'PROVEEDOR', true),
-    (4, 'fabricante@unlam.edu.ar', 'fabricante', 'FABRICANTE', true);*/
