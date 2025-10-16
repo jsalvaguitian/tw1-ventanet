7e38ce29234918371c79ea46c78c6171df808bb9
jsalvaguitian <jsalvaguitian@frba.utn.edu.ar>
2025-10-16 13:26:20 -0300

    merge

---
+++ src/main/java/com/tallerwebi/presentacion/ViewController.java
@@ -0,0 +1,28 @@
+/*package com.tallerwebi.presentacion;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    public ViewController() {
+        System.out.println(">>> ViewController instanciado <<<");
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";  // apunta a /WEB-INF/views/thymeleaf/login.html
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard";
+    }
+    
+       @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
+*/
