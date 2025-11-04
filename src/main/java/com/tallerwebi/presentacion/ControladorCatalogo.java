0f8d03f89eb17fce6df4c7be903f27b63b068bab
jsalvaguitian <jsalvaguitian@frba.utn.edu.ar>
2025-11-04 18:42:35 -0300

    actualizo el dockerfile jetty para que me reconozca el jdk, cambio las dependencias de javaxmail por jakarta mail para que funcione con docker-compose y arregle un bug de detalle de producto

--- src/main/java/com/tallerwebi/presentacion/ControladorCatalogo.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorCatalogo.java
@@ -1,10 +1,7 @@
 package com.tallerwebi.presentacion;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -24,7 +21,6 @@ import com.tallerwebi.dominio.entidades.TipoProducto;
 import com.tallerwebi.dominio.entidades.TipoVentana;
 import com.tallerwebi.dominio.enums.Rubro;
 import com.tallerwebi.dominio.servicios.ServicioMarca;
-import com.tallerwebi.dominio.servicios.ServicioProdV2;
 import com.tallerwebi.dominio.servicios.ServicioProducto;
 import com.tallerwebi.dominio.servicios.ServicioProveedorI;
 import com.tallerwebi.dominio.servicios.ServicioTipoProducto;
