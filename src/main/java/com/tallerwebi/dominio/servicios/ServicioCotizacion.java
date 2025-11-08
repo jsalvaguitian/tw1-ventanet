3f21d1682e61acd28498421a7a113e9e9daa2fb3
Sergio Gabriel Rubio <serrubio@publicisgroupe.net>
2025-11-08 00:45:10 -0300

    arreglo filtro de periodos de dashboard cliente

--- src/main/java/com/tallerwebi/dominio/servicios/ServicioCotizacion.java
+++ src/main/java/com/tallerwebi/dominio/servicios/ServicioCotizacion.java
@@ -7,9 +7,10 @@ import com.tallerwebi.dominio.entidades.Cliente;
 import com.tallerwebi.dominio.entidades.Cotizacion;
 import com.tallerwebi.dominio.enums.EstadoCotizacion;
 import com.tallerwebi.dominio.excepcion.CotizacionesExistente;
+import com.tallerwebi.dominio.excepcion.NoHayCotizacionExistente;
 
 public interface ServicioCotizacion {
-    Cotizacion obtenerPorId(Long id);
+    Cotizacion obtenerPorId(Long id) throws NoHayCotizacionExistente;
     List<Cotizacion> obtenerPorIdProveedor(Long proveedorId);
     void actualizarEstado(Long cotizacionId, EstadoCotizacion estado)throws CotizacionesExistente;
     List<Cotizacion> obtenerCotizacionPorIdCliente(Long id);
